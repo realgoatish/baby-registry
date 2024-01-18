e4cad726f1b46f9be7260531d63df166f2e83e77
Mike Lamb <mflambjr@gmail.com>
2024-01-17 21:43:27 -0500

    Vercel edge config

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,8 +1,12 @@
 import { redirect } from "@sveltejs/kit";
 
+export const config = {
+  runtime: "edge",
+};
+
 export function load() {
   redirect(
-    307,
+    308,
     "https://www.amazon.com/baby-reg/sahar-lamb-michael-lamb-june-2024-mountlaurel/1CV2HUWO98BF0"
   );
 }
